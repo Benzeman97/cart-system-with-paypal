8230fc13acea0049d0fbd9c91c7121b9b6a963b6
Benz <Benzeman097-nafazbenzema@gmail.com>
2020-07-25 12:46:13 +0530

    styled-components added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,16 +3,19 @@ import {Link} from 'react-router-dom';
 import shopping from '../shopping.png';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faCartPlus} from '@fortawesome/free-solid-svg-icons';
+import {ButtonContainer} from '../style-components/Button';
+import styled from 'styled-components';
 
 class Navbar extends Component {
     render() {
         return (
-           <nav className="navbar navbar-expand-sm bg-primary navber-dark px-sm-5">
+           <NavWarapper className="navbar navbar-expand-sm navber-dark px-sm-5">
 
            <Link to="/">
            <img src={shopping} alt="shopping-logo" className="navbar-brand"/>
 
            </Link>
+         
           <ul className="navbar-nav align-items-center">
 
               <li className="nav-item ml-5">
@@ -21,6 +24,7 @@ class Navbar extends Component {
                   </Link>
               </li>
           </ul>
+       
           <ul className="navbar-nav align-items-center">
 
           <li className="nav-item ml-5">
@@ -30,18 +34,30 @@ class Navbar extends Component {
               </li>
           </ul>
          
+         
          <Link to="/cart" className="ml-auto">
-           <button>
+           <ButtonContainer>
+               <span className="mr-2">
            <FontAwesomeIcon icon={faCartPlus} />
+             </span>
            my cart
-           </button>
+           </ButtonContainer>
       
          </Link>
-        
-
-           </nav>
+           </NavWarapper>
         );
     }
 }
+
+const NavWarapper = styled.nav`
+   background:var(--mainBlue);
+   .nav-link{
+       color: var(--mainWhite);
+       text-transform:capitalize;
+       font-size:1.3rem;
+   }
+`;
+
+
 
 export default Navbar;
